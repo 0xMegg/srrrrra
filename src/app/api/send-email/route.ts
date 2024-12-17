1b992526a084c76a695bb950232ae67c499baeda
Megg <finero91@gmail.com>
2024-12-17 20:56:52 +0900

    mailing service complete

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -11,12 +11,8 @@ export async function POST(req: Request) {
       port: 465,
       secure: true,
       auth: {
-        type: "OAuth2",
         user: process.env.EMAIL_USER,
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-        accessToken: process.env.GMAIL_ACCESS_TOKEN,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
