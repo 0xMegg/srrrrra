d67dc4e50a1ef0d001fed8e087de37a6a2271d63
Megg <finero91@gmail.com>
2025-01-25 12:49:09 +0900

    fix robot.txt

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 사이트맵 설정 추가
+  // 사이트맵과 robots.txt 설정 추가
   async rewrites() {
     return [
       {
         source: "/sitemap.xml",
         destination: "/public/sitemap.xml",
+      },
+      {
+        source: "/robots.txt",
+        destination: "/public/robots.txt",
       },
     ];
   },
